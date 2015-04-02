d6e4f636515277cce1cc44984b1fcc113aa229ec
Jorden Gysbrechts <jorden.gysbrechts.v4563@student.hogent.be>
2015-04-02 17:32:52 +0200

    update

---
+++ Portfolio/public_html/js/citaten.js
@@ -0,0 +1,160 @@
+/* Spreuken, voor o.a. 'spreuk van de dag'
+   Verzameld in 2002 - 2006 door Ben Boukes
+   Mag vrij worden gebruikt, mits dit commentaar niet wordt verwijderd
+*/
+var spreuk = new Array();
+var naam = new Array();
+var plaats = new Array(); // 0 is linksboven met een : , 1 is rechtsonder zonder :
+
+/* Voeg spreuken toe en of verwijder spreuken naar eigen inzicht. */
+/* Maximaal 365 spreuken, anders werkt het niet meer */
+spreuk[0] = "Onderzoek doe ik wanneer ik niet weet wat ik doe."
+naam[0] = "Het credo van Von Braun"; plaats[0] = 0;
+
+spreuk[1] = "Een goede planning is geen vervanging voor stom geluk."
+naam[1] = "De wet van Dunn"; plaats[1] = 0;
+
+spreuk[2] = "Loop nooit door de gang van een kantoorgebouw zonder een stuk papier in je hand.";
+naam[2] = "De eerste wet van het zakenleven"; plaats[2] = 0;
+
+spreuk[3] = "Voer nooit onderhandelingen voor tien uur 's ochtends of na vier uur 's middags.<br>Voor tien uur lijk je te gretig en na vier uur denken ze dat je wanhopig bent.";
+naam[3] = "De tweede wet van het zakenleven"; plaats[3] = 0;
+
+spreuk[4] = "Het is gemakkelijk de zonnige kanten te zien van de problemen van anderen.";
+naam[4] = "De wet van McGuffin"; plaats[4] = 0;
+
+spreuk[5] = "Besluiteloosheid is de sleutel tot flexibiliteit.";
+naam[5] = "De observatie van Mullins"; plaats[5] = 0;
+
+spreuk[6] = "Surfen op het internet is hetzelfde als een dag spenderen voor een rek met tijdschriften.";
+naam[6] = "Het equivalent van Erickson"; plaats[6] = 0;
+
+spreuk[7] = "Geen enkele baas zal een werknemer in dienst houden die de hele tijd gelijk heeft.";
+naam[7] = "De eerste valkuil voor een genie"; plaats[7] = 0;
+
+spreuk[8] = "Ik denk nooit aan de toekomst. Zij komt gauw genoeg";
+naam[8] = "Albert Einstein"; plaats[8] = 0;
+
+spreuk[9] = "Het is een vergissing een mechanisch voorwerp te laten merken dat je haast hebt.";
+naam[9] = "De observatie van Ralph"; plaats[9] = 0;
+
+spreuk[10] = "Een expert is iemand die binnen een beperkt vakgebied alle mogelijke fouten heeft gemaakt.";
+naam[10] = "De wet van Bohr"; plaats[10] = 0;
+
+spreuk[11] = "On-line technische ondersteuning is bedacht om van alles te verstrekken, behalve hulp.";
+naam[11] = "Het postulaat van Perkins"; plaats[11] = 0;
+
+spreuk[12] = "Wie trots is op zijn wijsheid heeft van wijsheid niets begrepen. Zo iemand is niet wijs."
+naam[12] = "De waarneming van Ben"; plaats[12] = 0;
+
+spreuk[13] = "Lees de bijbel niet als een notaris, maar als een erfgenaam.";
+naam[13] = "Ds. A. v.d. Veer"; plaats[13] = 1;
+
+spreuk[14] = "De menselijke geest gelooft het liefst onbegrijpelijke dingen.";
+naam[14] = "Blaise Pascal"; plaats[14] = 1;
+
+spreuk[15] = "Het geweten is Gods aanwezigheid in de mens.";
+naam[15] = "Emanu&euml;l Swedenborg"; plaats[15] = 1;
+
+spreuk[16] = "Ik ontdekte dat ik minder en minder te zeggen had, tot ik uiteindelijk stil werd en begon te luisteren. En in die stilte hoorde ik de stem van God. ";
+naam[16] = "S&oslash;ren Kierkegaard"; plaats[16] = 1;
+
+spreuk[17] = "Onrustig is ons hart totdat het vrede vindt in God.";
+naam[17] = "Augustinus"; plaats[17] = 1;
+
+spreuk[18] = "Flexibiliteit is iets anders dan van je medemens te verlangen dat hij zich aan jou aanpast omdat je eigen agenda van elastiek is.";
+naam[18] = " De waarneming van Rick"; plaats[18] = 0;
+
+spreuk[19] = "Het is eenvoudiger een stelling te weerleggen dan er een op te stellen.";
+naam[19] = "Aristoteles"; plaats[19] = 1;
+
+spreuk[20] = "Alles wat fout kan gaan, gaat een keer fout.";
+naam[20] = "De wet van Murphy"; plaats[20] = 0;
+
+spreuk[21] = "Het duurt altijd langer dan je denkt.";
+naam[21] = "De wet van Hofstadter"; plaats[21] = 1;
+
+spreuk[22] = "Ook PC's zijn gevoelig voor stress; ze vertonen de meeste storingen als ze het hardst nodig zijn.";
+naam[22] = "Maarten W.P.C. van Rossum"; plaats[22] = 1;
+
+spreuk[23] = "Rust is er niet, die moet je zoeken.";
+naam[23] = "Robert Beek"; plaats[23] = 1;
+
+spreuk[24] = "Men moet nooit te lui zijn om het gemak te blijven zoeken.";
+naam[24] = "Anonymus"; plaats[24] = 1;
+
+spreuk[25] = "De dwaas doet wat hij niet laten kan.<br>De Wijze laat wat hij niet doen kan.";
+naam[25] = "Oosterse wijsheid"; plaats[25] = 1;
+
+spreuk[26] = "Vervloek de duisternis niet, maar steek een kaars aan.";
+naam[26] = "Chinees spreekwoord"; plaats[26] = 1;
+
+spreuk[27] = "Werk, werk van vroeg tot laat. Ik heb zoveel te doen, dat ik de eerste drie uur in gebed zal doorbrengen.";
+naam[27] = "Maarten Luther"; plaats[27] = 1;
+
+spreuk[28] = "Vaak hebben wij ruzie met anderen omdat wij ten diepste ontevreden zijn over onszelf.";
+naam[28] = "William Hazlitt"; plaats[28] = 1;
+
+spreuk[29] = "Het is beter vragen te stellen dan antwoorden te weten.";
+naam[29] = "James Thurber"; plaats[29] = 1;
+
+spreuk[30] = "Waarachtig geluk is: genieten van het heden zonder bang te zijn voor de toekomst.";
+naam[30] = "Lucius Annaeus Seneca"; plaats[30] = 1;
+
+spreuk[31] = "Mannen willen maar &eacute;&eacute;n ding: Auto's, carri&egrave;re, status, voetbal, bier, enz.";
+naam[31] = "Rikkert Zuiderveld"; plaats[31] = 1;
+
+spreuk[32] = "Het kan onrust veroorzaken. Als u een rustige, kalme kerk wilt, moet u niet bidden om de Geest.";
+naam[32] = "prof.dr. C. van der Kooi"; plaats[32] = 1;
+
+spreuk[33] = "Van een schouderklopje is nog nooit iemand geblesseerd geraakt.";
+naam[33] = "Leo Beenhakker"; plaats[33] = 1;
+
+spreuk[34] = "Als je echt onwetend wilt blijven dan moet je daar echt je best voor doen!";
+naam[34] = "Ing. Bert Wijker"; plaats[34] = 1;
+
+spreuk[35] = "Christendom is geen religie.<br>Het is een relatie met de Levende God";
+naam[35] = "A. Aartsma"; plaats[35] = 1;
+
+/* Hieronder niet wijzigen */
+dagen = new Array(31,28,31,30,31,30,31,31,30,31,30,31);
+vandaag = new Date();
+maand = vandaag.getMonth();
+geweest = vandaag.getDate();
+for (i=0;i<maand;i++)
+  geweest += dagen[i];
+if (maand > 1) {  // schrikkeljaar ?
+  jaar = vandaag.getYear();
+  if ((jaar % 4) == 0) geweest += 1;
+}
+nummer = geweest % naam.length;
+/* Wijzig de (tabel-)opmaak in de functie naar eigen inzicht */
+function ToonCitaat(font_1, size_1, color_1, font_2, size_2, color_2) {
+  if (naam.length > 365) {     // Foutmelding
+    document.write('*** FOUT *** meer dan 365 spreuken in citaten.js');
+    return;
+  }
+  document.write('\<table width="90%" border="0" cellspacing="1" cellpadding="10" align="center">');
+
+  if (plaats[nummer] == 0) {
+  document.write('\<tr\>\<td align="left"\>');  // naam van de spreker
+  document.write('\<font face="',font_2,'" size="',size_2,'" color="',color_2,'"\>');
+  document.write(naam[nummer],':\</font\>');
+  document.write('\</td\>\</tr\>');
+  }
+
+  document.write('\<tr\>\<td align="center"\>');  // spreuk
+  document.write('\<font face="',font_1,'" size="',size_1,'" color="',color_1,'"\>');
+  document.write(spreuk[nummer],'\</font\>');
+  document.write('\</td\>\</tr\>');
+
+  if (plaats[nummer] == 1) {
+  document.write('\<tr\>\<td align="right"\>');  // naam van de spreker
+  document.write('\<font face="',font_2,'" size="',size_2,'" color="',color_2,'"\>');
+  document.write(naam[nummer],'\</font\>');
+  document.write('\</td\>\</tr\>');
+  }
+
+  document.write('\</table\>');
+}
+
